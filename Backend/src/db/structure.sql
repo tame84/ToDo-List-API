c7451b2f1c8e623347bb0f4c9af80a76be6935e6
tame84 <146751584+tame84@users.noreply.github.com>
2024-08-31 10:23:54 +0200

    db: created database structure

---
+++ Backend/src/db/structure.sql
@@ -0,0 +1,62 @@
+-- SQLite
+PRAGMA foreign_keys = ON;
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS todos;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
+    username VARCHAR(16) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS categories (
+    id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
+    title VARCHAR(32) NOT NULL,
+    description VARCHAR(255),
+    user_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS todos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
+    title VARCHAR(140) NOT NULL,
+    content TEXT,
+    isCompleted BOOLEAN DEFAULT FALSE NOT NULL,
+    user_id INTEGER,
+    category_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
+);
+
+INSERT INTO users (
+    username,
+    password
+) VALUES (
+    "tame84",
+    "$argon2id$v=19$m=65536,t=3,p=4$sNV95gEtzin4HETSmSLX/g$tlO5p0d/ymzFqQYLGx4fl3Pg8NuSWgjltruSMg+lZpQ"
+);
+
+INSERT INTO categories (
+    title,
+    description,
+    user_id
+) VALUES (
+    "IRL",
+    "Tâches à faire IRL",
+    1
+);
+
+INSERT INTO todos (
+    title,
+    content,
+    user_id,
+    category_id
+) VALUES (
+    "Faire les courses",
+    "Liste de courses: bananes, oeufs, lait, steak, pain",
+    1,
+    1
+);
+
